bf4b0f9a749a87a0fb46458e226784da57599093
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-05-05 09:32:26 +0700

    New Base Style

--- app/src/main/java/com/gzeinnumer/mybasecode/utils/GblFunction.java
+++ app/src/main/java/com/gzeinnumer/mybasecode/utils/GblFunction.java
@@ -97,4 +97,21 @@ public class GblFunction {
 
         return fileSizeInMB <= 20;
     }
+
+    /**
+     * "data:image/jpeg;"
+     */
+    public static String convertToBase64WithDataImage(String imagePath) {
+        try {
+            Bitmap bm = BitmapFactory.decodeFile(imagePath);
+            //  bm = Bitmap.createScaledBitmap(bm, 1024, 1024, false);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bm.compress(Bitmap.CompressFormat.JPEG, 25, baos);
+            byte[] byteArrayImage = baos.toByteArray();
+            return "data:image/jpeg;base64," + Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+        } catch (Exception e) {
+            Log.d(TAG, "convertToBase64WithDataImage: " + e.getMessage());
+            return " ";
+        }
+    }
 }
